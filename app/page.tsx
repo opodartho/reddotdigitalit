97a732ac69d2ab9c7cd208ceccabbe2bdff5d73d
TasinNazifa <2252421047@student.bup.edu.bd>
2025-08-11 23:34:16 +0600

    v4

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import  {TabButtons } from "@/components/ui/TabButtons";
 
 export default function Home() {
   return (
-    <main><h1 className="text-center font-bold pt-7">Who We Are</h1>
-    <p className="text-center"> Our cutting-edge Modular Data Center solutions enable to protect mission-critical data</p>
+    <main><h1 className="text-center font-bold text-2xl pt-7">Who We Are</h1>
+    <p className="text-center pt-2"> Our cutting-edge Modular Data Center solutions enable to<br></br> protect mission-critical data</p>
     <TabButtons />
     </main>
   )
