4226e0f7d1225b7c409ee2cb4b36de112b1d7102
shanjidaaf <shanjida87bup@gmail.com>
2025-08-11 12:21:33 +0600

    Added button variants of homepage
    -Added roundedRedOutline, curvedRedOutline, and default button variants with distinct styles.

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -7,26 +7,29 @@ import { cn } from "@/lib/utils"
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
-    variants: {
-      variant: {
-        roundedRedOutline:
-          "border border-red-500 text-red-500 bg-white hover:bg-red-50 dark:bg-transparent rounded-full",
-        curvedRedOutline:
-          "border border-red-500 text-red-500 bg-white hover:bg-red-50 dark:bg-transparent rounded-md",
-      },
-      size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 px-6 has-[>svg]:px-4",
-        icon: "size-9",
-      },
+  variants: {
+    variant: {
+      roundedRedOutline:
+        "border border-red-600 text-red-600 bg-white hover:bg-red-200 dark:bg-transparent rounded-full",
+      curvedRedOutline:
+        "border border-red-600 text-red-600 bg-white hover:bg-red-200 dark:bg-transparent rounded-md",
+      default:
+        "text-white bg-red-600 hover:bg-primary/20 dark:bg-transparent rounded-md"
     },
-    defaultVariants: {
-      variant: "roundedRedOutline",
-      size: "default",
-    },
+    size: {
+      default: "h-9 px-8 py-2 has-[>svg]:px-7",
+      sm: "h-8 gap-1.5 px-7 has-[>svg]:px-6",
+      lg: "h-10 px-10 has-[>svg]:px-8",
+      icon: "size-9"
+    }
+  },
+  defaultVariants: {
+    variant: "roundedRedOutline",
+    size: "default"
   }
-)
+}
+);
+
 
 function Button({
   className,
