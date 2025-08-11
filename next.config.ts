26ee4cfa2fe60c7aed0740c1d8b4d6bd83cb3651
Zahidul Haque <iisowrovii@gmail.com>
2025-08-12 01:10:12 +0600

    Product solution image modification

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
        {
         protocol: 'https',
-        hostname: 'images.unsplash.com', // The domain for Unsplash images
+        hostname: 'images.unsplash.com', 
         port: '',
         pathname: '/**',
       },
