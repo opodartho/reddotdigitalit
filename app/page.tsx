d7276e3f6d0e2957963135b95edcd86c710c28d2
Zahidul Haque <iisowrovii@gmail.com>
2025-08-13 03:53:45 +0600

    Added red outline and curved button variants
    Created two new button variants with red outlines; one rounded, one with curved edges.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+<<<<<<< HEAD
 import React from "react";
 import { Landing } from "@/components/home/Landing";
 
 const Home = () => <Landing />;
 
 export default Home;
+=======
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="flex gap-4 pt-4 pl-2">
+      <Button variant="roundedRedOutline">Contact Us</Button>
+      <Button variant="curvedRedOutline">View More</Button>
+    </div>
+  );
+}
+>>>>>>> 58f2107 (Added red outline and curved button variants)
