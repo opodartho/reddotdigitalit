2d6fd40ce42312204b048bfe2c3619615f13c8ea
Nusrat Jahan Ridhi <nusratridhi16@gmail.com>
2025-08-12 22:18:10 +0600

    Updated Section component for testimonial section

---
+++ components/home/_testimonial/Section.tsx
@@ -0,0 +1,13 @@
+import { TestimonialCarousel } from './Carousel';
+
+const TestimonialSection = () => {
+  return (
+    <div className="bg-gradient-to-r from-pink-100 to-orange-50 font-sans py-16 px-4">
+      <div className="max-w-7xl mx-auto">
+        <TestimonialCarousel />
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialSection;
