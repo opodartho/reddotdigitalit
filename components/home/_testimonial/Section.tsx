431b367215ae8b258d67e68a515d728cbc750f46
Nusrat-Jahan-Ridhi <nusratridhi16@gmail.com>
2025-08-13 12:41:37 +0600

    updated testimonial section

--- components/home/_testimonial/Section.tsx
+++ components/home/_testimonial/Section.tsx
@@ -2,7 +2,7 @@ import { TestimonialCarousel } from "./Carousel";
 
 const TestimonialSection = () => {
   return (
-    <div className="bg-gradient-to-r from-pink-100 to-orange-50 px-4 py-16 font-sans">
+    <div className="bg-gradient-to-r from-pink-100 to-orange-50 px-4 py-16 font-sans mt-30 mb-10">
       <div className="mx-auto max-w-7xl">
         <TestimonialCarousel />
       </div>
