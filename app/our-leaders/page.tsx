73ea59a6eeded5d6b42bdbe8512acf6924279994
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-11-04 11:25:13 +0600

    add: our-leaders

---
+++ app/our-leaders/page.tsx
@@ -0,0 +1,8 @@
+import OurLeaders from "@/components/our-leaders";
+
+
+export default function Leaders(){
+    return(
+        <OurLeaders/>
+    )
+}
---
+++ lib/data/leadersData.ts
@@ -0,0 +1,20 @@
+
+export type Leaders={
+    id: number,
+    imageUrl: string,
+    name: string,
+    role: string,
+    description: string
+}
+
+export const LeadersData: Leaders={
+    id: 1,
+    imageUrl:'/images/leader.svg',
+    name:'Hasib Mustabsir',
+    role:'Chief Executive Officer & Managing Director',
+    description:`Hasib Mustabsir has taken over the role of RedDot Digital Limited’s Chief Executive Officer from October 2021. Prior to this, he served as Executive Vice President of Analytics Center of Excellence in Robi Axiata Limited. Under his leadership Robi was recognized as the AI Maturity and Big Data Analytics Maturity Champion by Axiata Analytics Council. He has led the team to win ‘Best use of Analytics & AI’ award in Axiata Leap Program 2020 and ‘Innovation Leadership Award’ by World Leadership Congress & Awards in 2021. 
+    
+    A home-grown leader, Mustabsir joined Robi as “Engineer, Planning” back in 2003 and has been a part of internal talent development program since 2010. He has 18 years of experience in Corporate Strategy, Market Operations and Technology Division. A mastermind of Robi’s 4G leadership, Mustabsir was recognized as Axiata Champion and Robi Champion in 2018, the highest recognition for performance in Axiata and Robi respectively. 
+    
+    A graduate from Bangladesh University of Engineering and Technology (BUET), Mustabsir completed his EMBA from BRAC University in flying colours with the distinction of being recognized as the ‘Gold Medalist’. Mustabsir has also received certification on “Advancing Next-Gen Leaders” from Harvard Business School.`
+}
