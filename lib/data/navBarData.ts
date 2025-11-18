63838c858b04b004288090ddce4b075b21ddd21c
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-11-18 12:56:23 +0600

    fixes in the navbar

--- lib/data/navBarData.ts
+++ lib/data/navBarData.ts
@@ -60,29 +60,81 @@ export const staticNavData: NavLink[] = [
     title: "Products",
     items: [
       {
-        title: "Product Suite 1",
-        href: "/products/product-suite-1",
-        description: "Our flagship software suite.",
+        title: "MFS",
+        href: "/customize-product/1",
+        description: "Unlock secure, fast, and convenient mobile transactions",
       },
       {
-        title: "Product Suite 2",
-        href: "/products/product-suite-2",
-        description: "Solutions for enterprise clients.",
+        title: "DFS",
+        href: "/customize-product/2",
+        description: `Empowering individuals and businesses with accessible, innovative financial tools.`,
       },
+      {
+        title: "HRMS",
+        href: "/customize-product/3",
+        description: `HRMS encompasses candidate application, selection process, recruiting and onboarding, employee life cycle management, payroll management`,
+      },
+      {
+        title: "E-Commerce",
+        href: "/customize-product/4",
+        description: `An online video streaming OTT platform that offers Live TV and VOD. `,
+      },
+      {
+        title: "IOT Backend",
+        href: "/customize-product/5",
+        description: `a device that detects the presence of gases in an area, often as part of a safety system. `,
+      },
+      {
+        title: "Streaming Solution App",
+        href: "/customize-product/6",
+        description: `An online video streaming OTT platform that offers Live TV and VOD. `,
+      },
+      {
+        title: "Customer Experience Solution",
+        href: "/customize-product/7",
+        description: `encompasses a strategic blend of technology, processes, and people designed to optimize every interaction a customer has `,
+      },
+      {
+        title: "Sales & Distribution",
+        href: "/customize-product/8",
+        description: `encompasses a strategic blend of technology, processes, and people designed to optimize every interaction a customer has `,
+      },
+
+
     ],
   },
   {
     title: "About RedDot",
     items: [
       {
-        title: "Our Story",
-        href: "/about/our-story",
-        description: "Learn about our journey and mission.",
+        title: "About Us",
+        href: "/about-us",
+        description: "Learn about our journey and vision",
       },
       {
-        title: "Our Team",
-        href: "/about/our-team",
-        description: "Meet the brilliant minds behind our success.",
+        title: "Board of Directors",
+        href: "/board-of-directors",
+        description: "Learn about our Board of Directors",
+      },
+      {
+        title: "Management Council",
+        href: "/management-council",
+        description: "Learn about our Management Council",
+      },
+      {
+        title: "Our Policy",
+        href: "/abac-policy",
+        description: "Learn about our policy",
+      },
+      {
+        title: "Life at RedDot",
+        href: "/Life_At_RedDot",
+        description: "Learn about our life here at RedDot",
+      },
+      {
+        title: "Whistleblowing",
+        href: "/whistle-blow",
+        description: "Learn about our Whistleblowing Policy",
       },
       {
         title: "Careers",
