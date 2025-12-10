0708bfebdc33322626e68bdec653ba44c305b994
Zabin <zabinhossain@iut-dhaka.edu>
2025-12-10 16:38:05 +0600

    navbar contact us button fixed

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -167,7 +167,7 @@ className={cn(
                     <Link
                       href="/contact-us"
                       className={cn(
-                        " flex justify-center items-center rounded-[25px] w-[144px] h-[44px] text-lg transition-all duration-300",
+                        " flex justify-center items-center rounded-4xl w-[144px] h-[44px] text-lg transition-all duration-300",
                         "bg-gradient-to-r from-[#E52445] via-[#BD1E39] to-[#7F1426] text-white hover:opacity-90 "
                       )}
                     >
--- components/home/_news/Section.tsx
+++ components/home/_news/Section.tsx
@@ -14,8 +14,8 @@ const News = ({ newsData }: NewsSectionProps) => {
         <h2 className="mb-[32px] sm:mb-[48px] text-center font-poppins text-[32px] font-semibold leading-[40px] tracking-[0.03px] text-title">
           Latest Events
         </h2>
-        <div className="max-w-[1440px] px-[16px] sm:px-[80px] mx-auto">
-        <div className="grid grid-cols-1 xl:gap-6 gap-0 sm:grid-cols-2 xl:grid-cols-4">
+        <div className="max-w-[1440px] px-[38px] sm:px-[80px] mx-auto">
+        <div className="grid grid-cols-1 xl:gap-6 gap-0 sm:grid-cols-2 xl:grid-cols-4 space-y-[32px]">
           {newsData.map((item) => (
             <NewsCard
               key={item.id}
