ee01ad2c3a1076ea5c674ce705405888c6f977ec
Zabin <zabinhossain@iut-dhaka.edu>
2025-12-18 16:34:31 +0600

    abc policy

--- app/abac-policy/page.tsx
+++ app/abac-policy/page.tsx
@@ -4,11 +4,12 @@ import { TabButtons } from "./TabsButtons"; // adjust path if needed
 
 export default function AbacPolicy() {
     return (
-        <div>
+        <div className="max-w-[1440px] mx-auto">
             {/* Breadcrumb */}
             
-
-            <div className="lg:pl-[80px] lg:pr-[80px] lg:pt-[160px] pt-[120px] pl-[16px] pr-[16px]">                <p className="text-[24px] font-bold pb-[20px]">ABAC Policy</p>
+            
+            <div className="px-[16px] sm:px-[80px] lg:pt-[160px] pt-[120px] ">     
+                           <p className="text-[24px] font-bold pb-[20px]">ABAC Policy</p>
 
             <TabButtons
                     english={
