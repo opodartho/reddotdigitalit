9aaafb34be9602ac3401b254ee5bee827e9ef2c3
Zabin <zabinhossain@iut-dhaka.edu>
2025-12-21 15:02:41 +0600

    newsdata

--- lib/data/newsData.ts
+++ lib/data/newsData.ts
@@ -8,30 +8,29 @@ export type NewsItem = {
 export const staticNewsData: NewsItem[] = [
   {
     id: 1,
-    title: "Cloud Migration",
+    title:"RedDot welcomed a group of talented students from",
     description:
       "Our cutting-edge Modular Data Center solutions enable to protect mission-critical data.",
     imageUrl: "/images/news1.svg",
   },
   {
     id: 2,
-    title: "DevOps",
-    description:
-      "We provide DevOps consultation to our clients that helps in automation and standardization.",
+        title:"RedDot Digital Celebrates H1’24 Townhall",
+        description:"We provide DevOps consultation to our clients that helps in automation and standardization.",
     imageUrl: "/images/news2.svg",
   },
   {
     id: 3,
-    title: "Resources",
-    description:
-      "Our Resource as a Service solution's offer seamless access to top-tier IT professionals, precisely.",
+        title:"RedDot Digital Celebrates H1’24 Townhall",
+        description:`Our Resource as a Service solution's
+        offer seamless access to top-tier 
+        IT professionals, precisely  .`,
     imageUrl: "/images/news3.svg",
   },
   {
     id: 4,
-    title: "QA as a Service",
-    description:
-      "Our QA as a Service offerings provide comprehensive testing solutions.",
+        title:"Announcing the launch of “Campaign Management...",
+        description:"Our QA as a Service offerings provide comprehensive testing solutions  .",
     imageUrl: "/images/news4.svg",
   },
 ];
