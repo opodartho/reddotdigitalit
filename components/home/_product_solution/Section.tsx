7cde04422643c439ab2371edc909791a8c51f0f2
Zabin <zabinhossain@iut-dhaka.edu>
2025-12-24 17:23:29 +0600

    view more removed

--- components/home/_product_solution/Section.tsx
+++ components/home/_product_solution/Section.tsx
@@ -15,11 +15,6 @@ const ProductSolution = ({ solutions }: ProductSolutionSectionProps) => {
     <main className="">
       <Solutions solutions={solutions} />
 
-      <div className="mb-16 lg:mt-8 flex justify-center">
-        <RedButton  className="w-[195px] h-[56px] text-[16px] " >
-          View More
-        </RedButton>
-      </div>
     </main>
   );
 };
